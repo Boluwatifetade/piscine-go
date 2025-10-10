be86550e4856942ac95a3ea8b8f90b8c08003b28
btade <boluwatifewisdom23@gmai.com>
2025-10-10 11:46:25 +0100

    comb2

--- printcomb2.go
+++ printcomb2.go
@@ -3,27 +3,28 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			f := j + 1
 			for k := i; k <= '9'; k++ {
-				for ; f <= '9'; f++ {
-					z01.PrintRune((i))
-					z01.PrintRune((j))
+				var l rune
+				if k == i {
+					l = j + 1
+				} else {
+					l = '0'
+				}
+				for ; l <= '9'; l++ {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
 					z01.PrintRune(' ')
-					z01.PrintRune((k))
-					z01.PrintRune((f))
-					if i < '9' || j < '8' || k < '9' || f < '9' {
+					z01.PrintRune(k)
+					z01.PrintRune(l)
+					if !(i == '9' && j == '8' && k == '9' && l == '9') {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
-
 				}
-				f = '0'
 			}
 		}
 	}
 	z01.PrintRune('\n')
-
 }
