6409c22f56cc9e2b4459fba5ad3eb20512d7b98a
btade <boluwatifewisdom23@gmai.com>
2025-10-10 11:33:44 +0100

    comb2

--- printcomb2.go
+++ printcomb2.go
@@ -6,13 +6,13 @@ func PrintComb2() {
 	for num1 := 0; num1 <= 98; num1++ {
 		for num2 := num1 + 1; num2 <= 99; num2++ {
 
-			z01.PrintRune(rune('0' + (num1 / 10)))
-			z01.PrintRune(rune('0' + (num1 % 10)))
+			z01.PrintRune('0' + (num1 / 10))
+			z01.PrintRune('0' + (num1 % 10))
 
 			z01.PrintRune(' ')
 
-			z01.PrintRune(rune('0' + (num2 / 10)))
-			z01.PrintRune(rune('0' + (num2 % 10)))
+			z01.PrintRune('0' + (num2 / 10))
+			z01.PrintRune('0' + (num2 % 10))
 
 			if num1 != 98 || num2 != 99 {
 				z01.PrintRune(',')
