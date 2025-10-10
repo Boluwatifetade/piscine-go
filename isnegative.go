22bdecac4aebd2b7bfbe72f6c0205f2efc82cfc9
btade <boluwatifewisdom23@gmai.com>
2025-10-10 10:41:46 +0100

    negative

--- isnegative.go
+++ isnegative.go
@@ -2,12 +2,11 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func isNegative(nb int) {
+func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
 	} else if nb >= 0 {
 		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
-	return
 }
